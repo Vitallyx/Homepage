e628ef8459272febf26f33ca5c70af8b859a8be4
Bryan Muller <71022676+Vitallyx@users.noreply.github.com>
2022-07-10 21:14:18 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const video = document.querySelector('video')
+const next = document.querySelector('.continue')
+
+document.querySelectorAll('.intro').forEach(element => {
+    element.addEventListener('click', function() {
+        document.querySelector('main').style.display = 'none';
+        x = 0
+        clearInterval(timer);
+        timer = setTimeout(door, 1000);
+    })
+});
+
+let x = 0;
+let timer = setTimeout(door, 1000);
+next.hidden = true
+
+function door() {
+    if (x == 5) {
+        video.classList.add('standby');
+        next.hidden = true
+        clearInterval(timer);
+    }
+    if (x == 60) {
+        document.querySelector('main').style.display = 'flex';
+        clearInterval(timer);
+    } else {
+        x++;
+        timer = setTimeout(door, 1000);
+    }
+    console.log(x)
+}
+
+window.addEventListener('mousemove', function() {
+    video.classList.remove('standby');
+    video.style.filter = "brightness(20%)";
+    next.hidden = false
+    clearInterval(timer);
+    timer = setTimeout(door, 1000);
+    x = 0;
+})
+timer
